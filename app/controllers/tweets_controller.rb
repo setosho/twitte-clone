a6999c642a06a393b0cda9dc6f3c3eb37fd74d72
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-09-18 23:59:56 +0900

    一覧画面を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,20 @@
 class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to new_tweet_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :content)
   end
 end
