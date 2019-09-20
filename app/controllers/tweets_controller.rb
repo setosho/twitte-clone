eaf0a0b7f9eb7aaab9a14f7d96fee141ff24e174
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-09-20 18:13:07 +0900

    共通処理のメソッド化&パーシャルの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :update]
   def index
     @tweets = Tweet.all
   end
@@ -17,11 +18,11 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
+    #@tweet = Tweet.find(params[:id])
   end
 
   def update
-    @tweet = Tweet.find(params[:id])
+    #@tweet = Tweet.find(params[:id])
     if @tweet.update(tweet_params)
       redirect_to tweets_path, notice: "ツイートを編集しました"
     else
@@ -33,5 +34,9 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:title, :content)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
